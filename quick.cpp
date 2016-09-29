8302ddfc82e0bf068e7f5149ad89e8f9b25bc744
Emily Claire Obaditch <eobaditc@student02.cse.nd.edu>
2016-09-29 13:21:38 -0400

    updated

--- quick.cpp
+++ quick.cpp
@@ -14,7 +14,7 @@ Node *concatenate(Node *left, Node *right);
 
 void quick_sort(List &l, bool numeric) {
 
-
+    dump_node(l.head); 
     if(numeric){
         l.head = qsort(l.head, node_number_compare); 
     }
@@ -28,18 +28,23 @@ Node *qsort(Node *head, CompareFunction compare) {
     Node * pivot = head; 
     Node * left = nullptr;  
     Node * right = nullptr; 
-
+    
+    if(head != nullptr){
+        std::cout<<head->string<<"\n"; 
+    }
     //base case 
     if (head == nullptr || head ->next == nullptr){
+        std::cout<<"Base case\n"; 
         return head; 
     }
 
     //divide
-
+    dump_node(head) ;
     partition(head, pivot, left, right, compare); 
 
     //conquer
-    left = qsort(left, compare); 
+    left = qsort(left, compare);
+    std::cout<<"after right\n"; 
     right = qsort(right, compare); 
 
     return concatenate(left, right); 
@@ -48,17 +53,41 @@ Node *qsort(Node *head, CompareFunction compare) {
 void partition(Node *head, Node *pivot, Node *&left, Node *&right, CompareFunction compare) {
     
     Node * curr = head; 
-    Node * iterator = head; 
-
+    Node * iterator = head;
+    int count =0; 
+    int count2 = 0;
+    std::cout<<"partition\n"; 
     while(iterator != nullptr && iterator->next !=nullptr){
         curr = iterator; 
         iterator = iterator->next; 
         std::cout<<"stuck while\n"; 
+        std::cout<<curr->string<<"\n"; 
         if (compare(curr, pivot)){
-            curr->next = head; 
+            if (count == 0 ){
+                curr->next = nullptr; 
+                count++; 
+            }
+            else{
+            curr->next = head;
+            count++; 
+            }
             head = curr; 
+            //pivot->next = iterator;
         }
+        else{
+            if (count2 == 0){
+                curr->next = nullptr;
+                count2++;
+            }
+            else{
+                curr->next = pivot->next;
+                count2++;
+            }
+            pivot->next = curr;
+        }
+
     }
+    iterator = nullptr;
     left = head; 
     right = pivot; 
 
