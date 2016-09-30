9aa3236a9059fb8b259c4859dd6abe607a56dabb
Lauren Christine Ferrara <lferrara@student00.cse.nd.edu>
2016-09-30 10:26:43 -0400

    updated partition

--- quick.cpp
+++ quick.cpp
@@ -29,9 +29,7 @@ Node *qsort(Node *head, CompareFunction compare) {
     
     //base case 
     if (head == nullptr || head ->next == nullptr){
-        std::cout << "base case\n";
         return head; 
-
     }
 
     //divide
@@ -41,14 +39,20 @@ Node *qsort(Node *head, CompareFunction compare) {
     left = qsort(left, compare);
     right = qsort(right, compare); 
 
-    return concatenate(left, right); 
+    head = concatenate(left, right); 
+    return head;
 }
 
 void partition(Node *head, Node *pivot, Node *&left, Node *&right, CompareFunction compare) {
-    
+   
+    left = nullptr;
+    right = nullptr;
+ 
     Node * curr = head; 
-    while(curr != nullptr && curr->next != nullptr){
-        curr = curr->next; 
+    Node * temp = curr;
+
+    while(curr != nullptr){// && curr->next != nullptr){
+        temp = temp->next;
         
         if (compare(curr, pivot)){
             curr->next = left;
@@ -58,18 +62,22 @@ void partition(Node *head, Node *pivot, Node *&left, Node *&right, CompareFuncti
             curr->next = right;
             right = curr;
         }
+        curr = temp; 
     }
 
-    pivot->next = right;
-    right = pivot;
+    //pivot->next = right;
+    //right = pivot;
 }
 
 Node *concatenate(Node *left, Node *right) {
     Node * curr = left;
-    while(curr!=nullptr && curr->next != nullptr){
+    while(curr!=nullptr){// && curr->next != nullptr){
+        std::cout << curr->string << " curr string\n";
         curr = curr->next; 
     }
-    curr->next = right; 
+    if( right != nullptr ){
+        curr = right; //or right 
+    }
     dump_node(left);
     return left; 
 }
